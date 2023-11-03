ba87f4ce0f46ec3698bd642fc77df104a8d4595f
DELIRIO1 <jonas.ulquiorra@gmail.com>
2023-11-02 23:15:20 -0300

    Initial commit

---
+++ app/src/main/java/descomplica/desenvolvimentomobile/imccalculator/ResultadoActivity.kt
@@ -0,0 +1,44 @@
+package descomplica.desenvolvimentomobile.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ResultadoActivity : AppCompatActivity() {
+    private lateinit var textPeso: TextView
+    private lateinit var textAltura: TextView
+    private lateinit var textResultado: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+        textPeso = findViewById(R.id.text_peso)
+        textAltura = findViewById((R.id.text_altura))
+        textResultado = findViewById((R.id.text_resultado))
+
+        val bundle = intent.extras
+        if(bundle !=null){
+            val peso = bundle.getDouble("peso")
+            val altura = bundle.getDouble("altura")
+
+            textPeso.text = "Peso informado $peso kg"
+            textAltura.text = "Altura informado $altura m"
+
+            val imc = peso /(altura * altura)
+            if(imc <18.5){
+                textResultado.text="baixo"
+            }else if (imc in 18.5..24.9){
+                textResultado.text="Normal"
+
+            }else if(imc in 25.0 .. 29.9 ){
+                textResultado.text="SobrePeso"
+            }else{
+                textResultado.text ="Obeso"
+            }
+
+        }
+
+
+    }
+}
